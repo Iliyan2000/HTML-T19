6295d1292ce37b0cb00e9d8e03e817593001b55c
Iliyan <ili.2@abv.bg>
2022-01-02 21:44:08 +0200

    Next_commit

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,5 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
-  var hot = document.querySelectorAll('.hot');
-  hot.innerText.concat('🔥');
+  document.querySelectorAll('.hot')[0].innerText += "🔥";
 });
